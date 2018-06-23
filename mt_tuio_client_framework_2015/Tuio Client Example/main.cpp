37c468f54127316b32a5925f7becab6d6882dde3
Sven Schurig <sven.m.schurig@googlemail.com>
2018-06-23 17:26:15 +0200

    GEstenerkennung

--- mt_tuio_client_framework_2015/Tuio Client Example/main.cpp
+++ mt_tuio_client_framework_2015/Tuio Client Example/main.cpp
@@ -1,20 +1,40 @@
 #include <iostream>
 #include <process.h>
+#include <vector>
+#include<stdio.h>
+#include<stdlib.h>
+#include <time.h>
+#include <Windows.h>
 
 #include "TuioClient.h"
 #include "TuioListener.h"
 
 #include <SFML/Graphics.hpp>
 #include <GL/glut.h> // GLUT 3.0.0.1
-
-// NUGet SFML:	Install-Package sfml-window -Version 2.4.2 
-
+#include "..\geasture\include\GeometricRecognizer.h"
+#include "..\geasture\include\PathWriter.h"
 
 using namespace TUIO;
 using namespace std;
 using namespace sf;
+using namespace DollarRecognizer;
+
+utils util;
+int o;
+
+struct Pfade
+{
+	Path2D gesten[100];
+	bool gesetzt[100];
+
+};
+
+
+Pfade xGesten;
 
 TUIO::TuioClient *tuioClient; // global tuioClient for testing
+
+
 
 class Client : public TuioListener {
 	// these methods need to be implemented here since they're virtual methods
@@ -46,14 +66,29 @@ void draw()
 	tuioClient->unlockCursorList();
 
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT); // Bild Buffer löchen
+	
+	for (int i = 0; i < 100; i++) { // alle Finger auf nicht gesetzt setzen
+		xGesten.gesetzt[i] = false;
+	}
+	
 
 	for (std::list<TuioCursor*>::iterator cursorListIter = cursorList.begin(); cursorListIter != cursorList.end(); ++cursorListIter)
 	{
-		std::cout << "id : "<< (*cursorListIter)->getCursorID() << endl;
-		std::cout << "X : " << (*cursorListIter)->getX() << endl;
-		std::cout << "Y : " << (*cursorListIter)->getY() << endl;
 
-		// do things with the cursors	
+		int id = (int)(*cursorListIter)->getCursorID();
+		double x= (double)(*cursorListIter)->getX();
+		double y = (double)(*cursorListIter)->getY();
+
+		if (id >= 0) {
+			// Frames speichern
+
+			Point2D temp(x, y);
+			xGesten.gesten[id].push_back(temp); // Punkt speichern
+			xGesten.gesetzt[id] = true;			// geste noch nicht fertig = true 
+			
+			cout << "ID: " << id << "X: " << x << "Y: " << y << endl;
+
+		}
 
 		glBegin(GL_POINTS);
 		glColor3f(1, 0, 0);
@@ -62,9 +97,72 @@ void draw()
 
 
 	}
+	// alte Gesten löschen
+
+	for (int j = 0; j < 100; j++) {
+		if (!xGesten.gesetzt[j] && !xGesten.gesten[j].empty()) {
+			// Geste erkennen
+
+			GeometricRecognizer g;
+			g.loadTemplates();
+
+
+/*
+mögliche Gesten: 
+Arrow
+Caret
+CheckMark
+Circle
+Delete,
+Diamond
+LeftCurlyBrace
+LeftSquareBracket
+LeftToRightLine
+LineDownDiagonal
+Pigtail
+QuestionMark
+Rectangle
+RightCurlyBrace
+RightSquareBracket
+RightToLeftLine
+RightToLeftLine2
+RightToLeftSlashDown
+Spiral
+Star
+Triangle
+V
+X
+*/
+			vector<string> gesturesList;
+			gesturesList.push_back("Circle"); // Verfügbare Gesten
+			gesturesList.push_back("LeftToRightLine");
+			gesturesList.push_back("RightToLeftLine");
+			gesturesList.push_back("V");
+			gesturesList.push_back("Triangle");
+			gesturesList.push_back("CheckMark");
+			gesturesList.push_back("Spiral");
+			gesturesList.push_back("Rectangle");
+			gesturesList.push_back("0");		// muss immer am Ende sein
+			g.activateTemplates(gesturesList);
+
+			RecognitionResult r = g.recognize(xGesten.gesten[j]);
+			cout << "Recognized gesture: " << r.name << endl;
+			cout << "1$ Score:" << r.score << endl;
+			Sleep(500);
+
+
+			cout << "alten Fingerverlauf loeschen" << endl;
+
+			// Geste Ausgeben/Zeichnen
+			xGesten.gesten[j].clear(); // lösche Geste
+			
+		}
+		
+	}
 
 	glutSwapBuffers();
-	std::cout << "End Printing " << std::endl;
+	std::cout << "End Printing Frame " << std::endl;
+	//Sleep(50);
 }
 
 void tuioThread(void*)
@@ -85,22 +183,175 @@ void glInit()
 {	
 }
 
+MultiStrokeGesture getGestureP()
+{
+	MultiStrokeGesture MultiStrokes;
+	MultiStrokes.push_back(util.addPointsToMakePath(Point2D(507, 8), Point2D(507, 87)));
+	MultiStrokes.push_back(util.addPointsToMakePath(Point2D(510, 7), Point2D(528, 7), Point2D(530, 8), Point2D(544, 10), Point2D(550, 12), Point2D(550, 15), Point2D(558, 20), Point2D(560, 22), Point2D(561, 27), Point2D(562, 33), Point2D(561, 37), Point2D(559, 42), Point2D(556, 45), Point2D(550, 48), Point2D(544, 51), Point2D(538, 53), Point2D(532, 54), Point2D(525, 55), Point2D(519, 55), Point2D(513, 55), Point2D(510, 55)));
+	return MultiStrokes;
+}
+
+Path2D sampleGest()
+{
+	Path2D path;
+	path.push_back(Point2D(68, 222));
+	path.push_back(Point2D(70, 220));
+	path.push_back(Point2D(73, 218));
+	path.push_back(Point2D(75, 217));
+	path.push_back(Point2D(77, 215));
+	path.push_back(Point2D(80, 213));
+	path.push_back(Point2D(82, 212));
+	path.push_back(Point2D(84, 210));
+	path.push_back(Point2D(87, 209));
+	path.push_back(Point2D(89, 208));
+	path.push_back(Point2D(92, 206));
+	path.push_back(Point2D(95, 204));
+	path.push_back(Point2D(101, 201));
+	path.push_back(Point2D(106, 198));
+	path.push_back(Point2D(112, 194));
+	path.push_back(Point2D(118, 191));
+	path.push_back(Point2D(124, 187));
+	path.push_back(Point2D(127, 186));
+	path.push_back(Point2D(132, 183));
+	path.push_back(Point2D(138, 181));
+	path.push_back(Point2D(141, 180));
+	path.push_back(Point2D(146, 178));
+	path.push_back(Point2D(154, 173));
+	path.push_back(Point2D(159, 171));
+	path.push_back(Point2D(161, 170));
+	path.push_back(Point2D(166, 167));
+	path.push_back(Point2D(168, 167));
+	path.push_back(Point2D(171, 166));
+	path.push_back(Point2D(174, 164));
+	path.push_back(Point2D(177, 162));
+	path.push_back(Point2D(180, 160));
+	path.push_back(Point2D(182, 158));
+	path.push_back(Point2D(183, 156));
+	path.push_back(Point2D(181, 154));
+	path.push_back(Point2D(178, 153));
+	path.push_back(Point2D(171, 153));
+	path.push_back(Point2D(164, 153));
+	path.push_back(Point2D(160, 153));
+	path.push_back(Point2D(150, 154));
+	path.push_back(Point2D(147, 155));
+	path.push_back(Point2D(141, 157));
+	path.push_back(Point2D(137, 158));
+	path.push_back(Point2D(135, 158));
+	path.push_back(Point2D(137, 158));
+	path.push_back(Point2D(140, 157));
+	path.push_back(Point2D(143, 156));
+	path.push_back(Point2D(151, 154));
+	path.push_back(Point2D(160, 152));
+	path.push_back(Point2D(170, 149));
+	path.push_back(Point2D(179, 147));
+	path.push_back(Point2D(185, 145));
+	path.push_back(Point2D(192, 144));
+	path.push_back(Point2D(196, 144));
+	path.push_back(Point2D(198, 144));
+	path.push_back(Point2D(200, 144));
+	path.push_back(Point2D(201, 147));
+	path.push_back(Point2D(199, 149));
+	path.push_back(Point2D(194, 157));
+	path.push_back(Point2D(191, 160));
+	path.push_back(Point2D(186, 167));
+	path.push_back(Point2D(180, 176));
+	path.push_back(Point2D(177, 179));
+	path.push_back(Point2D(171, 187));
+	path.push_back(Point2D(169, 189));
+	path.push_back(Point2D(165, 194));
+	path.push_back(Point2D(164, 196));
+
+	return path;
+}
+
+Path2D getGestureArrow()
+{
+	Path2D path;
+	path.push_back(Point2D(68, 222));
+	path.push_back(Point2D(70, 220));
+	path.push_back(Point2D(73, 218));
+	path.push_back(Point2D(75, 217));
+	path.push_back(Point2D(77, 215));
+	path.push_back(Point2D(80, 213));
+	path.push_back(Point2D(82, 212));
+	path.push_back(Point2D(84, 210));
+	path.push_back(Point2D(87, 209));
+	path.push_back(Point2D(89, 208));
+	path.push_back(Point2D(92, 206));
+	path.push_back(Point2D(95, 204));
+	path.push_back(Point2D(101, 201));
+	path.push_back(Point2D(106, 198));
+	path.push_back(Point2D(112, 194));
+	path.push_back(Point2D(118, 191));
+	path.push_back(Point2D(124, 187));
+	path.push_back(Point2D(127, 186));
+	path.push_back(Point2D(132, 183));
+	path.push_back(Point2D(138, 181));
+	path.push_back(Point2D(141, 180));
+	path.push_back(Point2D(146, 178));
+	path.push_back(Point2D(154, 173));
+	path.push_back(Point2D(159, 171));
+	path.push_back(Point2D(161, 170));
+	path.push_back(Point2D(166, 167));
+	path.push_back(Point2D(168, 167));
+	path.push_back(Point2D(171, 166));
+	path.push_back(Point2D(174, 164));
+	path.push_back(Point2D(177, 162));
+	path.push_back(Point2D(180, 160));
+	path.push_back(Point2D(182, 158));
+	path.push_back(Point2D(183, 156));
+	path.push_back(Point2D(181, 154));
+	path.push_back(Point2D(178, 153));
+	path.push_back(Point2D(171, 153));
+	path.push_back(Point2D(164, 153));
+	path.push_back(Point2D(160, 153));
+	path.push_back(Point2D(150, 154));
+	path.push_back(Point2D(147, 155));
+	path.push_back(Point2D(141, 157));
+	path.push_back(Point2D(137, 158));
+	path.push_back(Point2D(135, 158));
+	path.push_back(Point2D(137, 158));
+	path.push_back(Point2D(140, 157));
+	path.push_back(Point2D(143, 156));
+	path.push_back(Point2D(151, 154));
+	path.push_back(Point2D(160, 152));
+	path.push_back(Point2D(170, 149));
+	path.push_back(Point2D(179, 147));
+	path.push_back(Point2D(185, 145));
+	path.push_back(Point2D(192, 144));
+	path.push_back(Point2D(196, 144));
+	path.push_back(Point2D(198, 144));
+	path.push_back(Point2D(200, 144));
+	path.push_back(Point2D(201, 147));
+	path.push_back(Point2D(199, 149));
+	path.push_back(Point2D(194, 157));
+	path.push_back(Point2D(191, 160));
+	path.push_back(Point2D(186, 167));
+	path.push_back(Point2D(180, 176));
+	path.push_back(Point2D(177, 179));
+	path.push_back(Point2D(171, 187));
+	path.push_back(Point2D(169, 189));
+	path.push_back(Point2D(165, 194));
+	path.push_back(Point2D(164, 196));
+
+	return path;
+}
 
 int main(int argc, char** argv)
 {
-
-	
+		
 	// create a second thread for the TUIO listener
 	HANDLE hThread_TUIO;
 	unsigned threadID;
 	//hThread = (HANDLE)_beginthreadex( NULL, 0, &tuioThread, NULL, 0, &threadID );
 	hThread_TUIO = (HANDLE)_beginthread( tuioThread, 0, NULL );
 
+	
 	// GLUT Window Initialization (just an example):
 	glutInit(&argc, argv);
-	glutInitWindowSize(752, 480);
+	glutInitWindowSize(800, 600);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DOUBLE | GLUT_DEPTH);
-	glutCreateWindow("MultiTouchGame Johannes, Sven ");
+	glutCreateWindow("MultiTouchGame Johannes, Sven");
 
 	// openGL init
 	glInit();
@@ -111,8 +362,6 @@ int main(int argc, char** argv)
 	glutMainLoop();
 	
 	
-	
-
-
 	return 0;
+
 }
